a133e922340ad510bc18c94f880da2dc2e61b799
vasile luta <elsile69@yahoo.com>
2020-01-15 15:55:58 +0200

    pug templating

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
+app.set('view engine', 'pug');
 
 const fruits = [
   { name: 'apple', quantity: 3, price: 5, unit: 'kg' },
@@ -12,45 +13,11 @@ const fruits = [
 ];
 
 app.get('/', (req, res) => {
-  let list = '';
-
-  for (let i = 0; i < fruits.length; i++) {
-    list +=
-      '<li>' +
-      '<a href="/' +
-      fruits[i].name +
-      '">' +
-      fruits[i].name +
-      '</a>' +
-      ' - ' +
-      fruits[i].price +
-      ' / ' +
-      fruits[i].unit +
-      ' - ' +
-      fruits[i].quantity +
-      '</li>';
-  }
-
   // list === ''
 
   // const mappedList = fruits.map((fruit) => '<li>' + fruit.name + '</li>').join('\n');
 
-  const response = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <title>Fruits store</title>
-    </head>
-    <body>
-      <h1>Buna ziua!</h1>
-      <h3>Bine ati venit!</h3>
-      <p>Puteti cumpara urmatoarele:</p>
-
-      <ul>${list}</ul>
-    </body>
-  </html>
-  `;
-  res.send(response);
+  res.render('index', { fruits });
 });
 
 const updateQuantity = (fruit, newQuantity) => {
@@ -73,15 +40,15 @@ app.post('/buy', (req, res) => {
     updateQuantity(fructulcautat, fructulcautat.quantity - cantitatea);
 
     const buyq = `
-  <html>
-    <head>
-      <title>buying page</title>
-    </head>
-    <body>
-      <a href="/">Home</a>
-      <p>Ai cumparat ${cantitatea} kile de ${fructulcautat.name}</p>
-    </body>
-  </html>
+      <html>
+        <head>
+          <title>buying page</title>
+        </head>
+        <body>
+          <a href="/">Home</a>
+          <p>Ai cumparat ${cantitatea} kile de ${fructulcautat.name}</p>
+        </body>
+      </html>
 `;
     res.send(buyq);
   } else {
